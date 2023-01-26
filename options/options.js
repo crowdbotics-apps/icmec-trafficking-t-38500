6781313bbed8b22ae5b6ffc556d134c0b18e4b7d
Crowdbotics <team@crowdbotics.com>
2023-01-26 21:02:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "icmec-trafficking-t-38500",
+  url: "https://icmec-trafficking-t-38500.botics.co",
+  api: "https://icmec-trafficking-t-38500.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
